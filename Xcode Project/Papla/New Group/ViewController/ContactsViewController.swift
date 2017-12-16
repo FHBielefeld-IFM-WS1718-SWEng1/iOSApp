5fb693c089bff135b2ae7e22b976365823b49083
Dario Leunig <daleunig@gmail.com>
2017-12-16 12:24:52 +0100

    ContactView implementiert (noch fehlerhaft)

--- Xcode Project/Papla/New Group/ViewController/ContactsViewController.swift
+++ Xcode Project/Papla/New Group/ViewController/ContactsViewController.swift
@@ -8,12 +8,50 @@
 
 import UIKit
 
-class ContactsViewController: UIViewController {
-
+class ContactsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    final let url = URL(string: "https://api.myjson.com/bins/d85sr")
+    private var contacts = [Contact]()
+    
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Initialisiert ein Aktualisieren des TableView Inhalts
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    lazy var refreshControl: UIRefreshControl = {
+        
+        let refreshControl = UIRefreshControl()
+        
+        refreshControl.addTarget(self, action:
+            #selector(ContactsViewController.handleRefresh(_:)),
+                                 for: UIControlEvents.valueChanged)
+        
+        refreshControl.tintColor = UIColor.darkGray
+        
+        return refreshControl
+    }()
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Wird durch refreshControl aufgerufen. Downloaded JSON erneut und lädt
+    // den Inhalt der TableView neu
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    @objc func handleRefresh(_ refreshControl: UIRefreshControl) {
+        downloadJSON()
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+        
+        refreshControl.endRefreshing()
+    }
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.tableView.addSubview(self.refreshControl)
+        downloadJSON()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +59,64 @@ class ContactsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Laedt JSON herrunter und parsed den Inhalt in Objekte
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    func downloadJSON() {
+        /* "guard" ist wie eine if-Abfrage, ist die Bedingung nicht erfuellt, wird die Funktion vorzeitig beendet.
+         * Beispiel:
+         *
+         * guard x < 150 else {
+         * return
+         * }
+         *
+         * Quelle: https://codingtutor.de/swift-2-0-guard-statements-so-validierst-du-variablen/
+         */
+        
+        //guard let url = URL(string: jsonUrlString) else { return }
+        guard let downloadURL = url else { return }
+        
+        /* URLSession -> Bietet zahlreiche Möglichkeiten mit Netzwerkprotokollen zu kommunizieren
+         * shared -> Lädt den Inhalt einer URL in den Speicher
+         * dataTask -> Ruft den Inhalt der URL auf
+         */
+        
+        URLSession.shared.dataTask(with: downloadURL) { data, urlResponse, error in
+            guard let data = data, error == nil, urlResponse != nil else {
+                print("something is wrong")
+                return
+            }
+            print("downloaded")
+            do
+            {
+                let decoder = JSONDecoder()
+                let downloadedContacts = try decoder.decode(Contacts.self, from: data)
+                self.contacts = downloadedContacts.contacts
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            } catch {
+                print("something wrong after downloaded")
+            }
+            }.resume()
     }
-    */
-
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Zaehlt die Anzahlt der durch den Parser erstelle Kontakte im Array
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Fuellt die TableView Zellen mit Inhalt
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell") as? ContactCell else {return UITableViewCell()}
+        
+        cell.nameLbl.text = contacts[indexPath.row].name
+        return cell
+    }
 }
