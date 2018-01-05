dc2b059f87e43101fb0eddbd0f517df9f7262759
Jendrik Müller <jendrik.mueller@fh-bielefeld.de>
2018-01-05 11:22:28 +0100

    Logout implementiert

--- Xcode Project/Papla/ViewController/OverviewViewController.swift
+++ Xcode Project/Papla/ViewController/OverviewViewController.swift
@@ -35,6 +35,38 @@ class OverviewViewController: UIViewController {
     }
     
     /**
+     Ausloggen des Benutzers und rückkehr zum Startbildschirm
+     JM
+    */
+    @IBAction func logoutButton(_ sender: Any) {
+        let headers = [
+            "Cache-Control": "no-cache",
+            "Postman-Token": "374d7602-d864-f029-bc2e-544308d23924"
+        ]
+        let myUrl :String = "http://api.dleunig.de/logout?api=" + myUser.key!
+        let request = NSMutableURLRequest(url: NSURL(string: myUrl)! as URL,
+                                          cachePolicy: .useProtocolCachePolicy,
+                                          timeoutInterval: 10.0)
+        request.httpMethod = "DELETE"
+        request.allHTTPHeaderFields = headers
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error)
+            } else {
+                let httpResponse = response as? HTTPURLResponse
+                print(httpResponse)
+                DispatchQueue.main.async {
+                    self.performSegue(withIdentifier: "logout", sender: self)
+                }
+            }
+        })
+        
+        dataTask.resume()
+    }
+    
+    /**
     Aktion beim Klick auf den Dashboard Button
     */
     @IBAction func showDashboard(_ sender: Any) {
